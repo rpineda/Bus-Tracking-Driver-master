716bb978cc6d2dbc94bf98c935757ee8932a0fe9
Lovish Verma <vermalovish1@gmail.com>
2017-05-22 12:25:21 +0530

    Initial commit

---
+++ app/src/main/java/com/sahurjt/bstdriver/Interfaces/Checkable.java
@@ -0,0 +1,13 @@
+package com.sahurjt.bstdriver.Interfaces;
+
+/**
+ * Created by Lovish_Verma on 28-03-2017.
+ */
+
+public interface Checkable {
+    public Boolean checked = false;
+
+    public Boolean isChecked();
+
+    public void setChecked(Boolean value);
+}
